43a5a665f03dcf93a2d9b5cdfb16ea8934cc7f28
Gad-Ongoro <gadongoro1@gmail.com>
2024-07-21 19:37:24 +0300

    fix: Updated profile query logic

--- client.js
+++ client.js
@@ -1,9 +1,53 @@
 document.getElementById('logout').addEventListener('click', function() {
     localStorage.removeItem('access');
     localStorage.removeItem('refresh');
+    localStorage.clear();
     window.location.href = 'login.html';
   });
-  
 
-
+  document.addEventListener('DOMContentLoaded', function() {
+    const userApiUrl = 'https://mk-services.onrender.com/services/api/users/';
+    const profileUrl = 'https://mk-services.onrender.com/services/api/profiles/';
+    const access = localStorage.getItem('access');
   
+    fetch(userApiUrl)
+    .then(response => {
+      try {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      } catch (error) {
+        console.error('Error fetching client information:', error);
+        throw error;
+      }
+    })
+    .then(data => {
+      const user_id = localStorage.getItem('user_id');
+      const user = data.filter(user => user.id == user_id)[0];
+      const clientNameElement = document.querySelector('.clientName');
+      const clientEmailElement = document.querySelector('.clientEmail');
+  
+      clientNameElement.innerHTML = `<strong>Username: </strong> ${user.username}`;
+      clientEmailElement.innerHTML = `<strong>Email: </strong> ${user.email}`;
+
+      return fetch(profileUrl, {
+        headers: {
+          'Authorization': `Bearer ${access}`
+        }
+      });
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(profileData => {
+      const clientPhoneElement = document.querySelector('.clientPhone');
+      clientPhoneElement.innerHTML = `<strong>Phone:</strong> ${profileData.phone}`;
+    })
+    .catch(error => {
+      console.error('Error fetching client information:', error);
+    });
+  });  
--- provider.js
+++ provider.js
@@ -69,10 +69,61 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Handle Logout Button
     document.getElementById('logout').addEventListener('click', function() {
-        window.location.href = 'login.html'; // Adjust according to your logout logic
+        localStorage.clear();
+        window.location.href = 'login.html';
     });
 });
 
+document.getElementById('logout').addEventListener('click', function() {
+    localStorage.removeItem('access');
+    localStorage.removeItem('refresh');
+    localStorage.clear();
+    window.location.href = 'login.html';
+});
 
+document.addEventListener('DOMContentLoaded', function() {
+    const userApiUrl = 'https://mk-services.onrender.com/services/api/users/';
+    const profileUrl = 'https://mk-services.onrender.com/services/api/profiles/';
+    const access = localStorage.getItem('access');
+    
+    fetch(userApiUrl, {
+      headers: {
+        'Authorization': `Bearer ${access}`
+      }
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+        const user_id = localStorage.getItem('user_id');
+        const user = data.filter(user => user.id == user_id)[0];
+        const providerNameElement = document.querySelector('.providerName');
+        const providerEmailElement = document.querySelector('.providerEmail');
+    
+        providerNameElement.innerHTML = `<strong>Username:</strong> ${user.username}`;
+        providerEmailElement.innerHTML = `<strong>Email:</strong> ${user.email}`;
 
-
+        // provider profile
+        return fetch(profileUrl, {
+            headers: {
+            'Authorization': `Bearer ${access}`
+            }
+        });
+        })
+        .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+        })
+        .then(profileData => {
+        const providerPhoneElement = document.querySelector('.providerPhone');
+        providerPhoneElement.innerHTML = `<strong>Phone:</strong> ${profileData.phone}`;
+        })
+        .catch(error => {
+        console.error('Error fetching provider information:', error);
+    });
+});
